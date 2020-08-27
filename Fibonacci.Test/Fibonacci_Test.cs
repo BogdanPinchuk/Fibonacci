a8aa69de513510468fcc1f6dc5c1ee32606fc36b
Bogdan <pinchuk.brus@windowslive.com>
2020-08-27 11:35:24 +0300

    Added some method for calculate Fibonacci number for array of data and unit test and calculate speed of calculate

--- Fibonacci.Test/Fibonacci_Test.cs
+++ Fibonacci.Test/Fibonacci_Test.cs
@@ -8,46 +8,71 @@ namespace Fibonacci.Test
     [TestClass]
     public class Fibonacci_Test
     {
-        private delegate long del_Fibonacci(int n);
+        private delegate T del_Fibonacci<T, K>(K n);
 
-
-        [TestMethod]
-        public void FibonacciBine_long_OneInputValue()
-            => Fibonacci_OneInputValue(Fibonacci.FibonacciBine_long);
-
-        [TestMethod]
-        public void FibonacciApproximation_long_OneInputValue()
-            => Fibonacci_OneInputValue(Fibonacci.FibonacciApproximation_long);
-
-        private static void Fibonacci_OneInputValue(del_Fibonacci fib)
+        private int[] Stub
         {
-            // avarrage
-            int minN = -8, maxN = 20;
+            get
+            {
+                int minN = -8, maxN = 20;
 
-            int[] stub = new int[maxN - minN + 1];
+                int[] stub = new int[maxN - minN + 1];
 
-            for (int i = 0; i < stub.Length; i++)
-                stub[i] = minN + i;
+                for (int i = 0; i < stub.Length; i++)
+                    stub[i] = minN + i;
 
-            var str = new StringBuilder("\nInput data:\n");
-            foreach (var i in stub)
-                str.Append(i).Append("\t");
-
-            long[] expected = new long[]
+                return stub;
+            }
+        }
+        private double[] Expected
+            => new double[]
             {
                 -21, 13, -8, 5, -3, 2, -1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21,
                 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765
             };
 
+
+        [TestMethod]
+        public void FibonacciBine_OneInputValue()
+            => Fibonacci_OneInputValue(Fibonacci.FibonacciBine);
+
+        [TestMethod]
+        public void FibonacciApproximation_OneInputValue()
+            => Fibonacci_OneInputValue(Fibonacci.FibonacciApproximation);
+
+        [TestMethod]
+        public void FibonacciBine_ArrayValue()
+            => Fibonacci_ArrayValue(Fibonacci.FibonacciBine);
+
+        [TestMethod]
+        public void FibonacciApproximation_ArrayValue()
+            => Fibonacci_ArrayValue(Fibonacci.FibonacciApproximation);
+        
+        [TestMethod]
+        public void FibonacciBine_Parallel()
+            => Fibonacci_ArrayValue(Fibonacci.FibonacciBine_Par);
+
+        [TestMethod]
+        public void FibonacciApproximation_Parallel()
+            => Fibonacci_ArrayValue(Fibonacci.FibonacciApproximation_Par);
+
+
+        private void Fibonacci_OneInputValue(del_Fibonacci<double, int> fib)
+        {
+            // avarrage
+            var str = new StringBuilder("\nInput data:\n");
+            foreach (var i in Stub)
+                str.Append(i).Append("\t");
+
             str.Append("\n\nExpected:\n");
-            foreach (var i in expected)
+            foreach (var i in Expected)
                 str.Append(i).Append("\t");
 
             // act
-            long[] actual = new long[stub.Length];
+            double[] actual = new double[Stub.Length];
 
-            for (int i = 0; i < stub.Length; i++)
-                actual[i] = fib(stub[i]);
+            for (int i = 0; i < Stub.Length; i++)
+                actual[i] = fib(Stub[i]);
 
             str.Append("\n\nActual:\n");
             foreach (var i in actual)
@@ -55,11 +80,78 @@ namespace Fibonacci.Test
 
             // assert
             for (int i = 0; i < actual.Length; i++)
-                Assert.AreEqual(expected[i], actual[i]);
+                Assert.AreEqual(Expected[i], actual[i]);
+
+            Debug.WriteLine(str.ToString());
+        }
+        
+        private void Fibonacci_ArrayValue(del_Fibonacci<double[], int[]> fib)
+        {
+            // avarrage
+            var str = new StringBuilder("\nInput data:\n");
+            foreach (var i in Stub)
+                str.Append(i).Append("\t");
+
+            str.Append("\n\nExpected:\n");
+            foreach (var i in Expected)
+                str.Append(i).Append("\t");
+
+            // act
+            double[] actual = fib(Stub);
+
+            str.Append("\n\nActual:\n");
+            foreach (var i in actual)
+                str.Append(i).Append("\t");
+
+            // assert
+            for (int i = 0; i < actual.Length; i++)
+                Assert.AreEqual(Expected[i], actual[i]);
 
             Debug.WriteLine(str.ToString());
         }
 
+
+        [TestMethod]
+        public void CompareFibMethods()
+        {
+            // avarrage
+            // act
+            Stopwatch timer = new Stopwatch();
+
+            timer.Start();
+            for (int i = 0; i < Stub.Length; i++)
+                Fibonacci.FibonacciBine(Stub[i]);
+            timer.Stop();
+
+            Debug.WriteLine($"\nBinet's formula: {timer.Elapsed.TotalMilliseconds} ms\n");
+
+            timer.Restart();
+            for (int i = 0; i < Stub.Length; i++)
+                Fibonacci.FibonacciApproximation(Stub[i]);
+            timer.Stop();
+            Debug.WriteLine($"Approximation formula: {timer.Elapsed.TotalMilliseconds} ms\n");
+
+            timer.Restart();
+            Fibonacci.FibonacciBine(Stub);
+            timer.Stop();
+            Debug.WriteLine($"Binet's formula (array): {timer.Elapsed.TotalMilliseconds} ms\n");
+
+            timer.Restart();
+            Fibonacci.FibonacciApproximation(Stub);
+            timer.Stop();
+            Debug.WriteLine($"Approximation formula (array): {timer.Elapsed.TotalMilliseconds} ms\n");
+
+            timer.Restart();
+            Fibonacci.FibonacciBine_Par(Stub);
+            timer.Stop();
+            Debug.WriteLine($"Binet's formula (parallel): {timer.Elapsed.TotalMilliseconds} ms\n");
+
+            timer.Restart();
+            Fibonacci.FibonacciApproximation_Par(Stub);
+            timer.Stop();
+            Debug.WriteLine($"Approximation formula (parallel): {timer.Elapsed.TotalMilliseconds} ms\n");
+
+        }
 
     }
 }
--- Fibonacci/Fibonacci.cs
+++ Fibonacci/Fibonacci.cs
@@ -6,6 +6,9 @@
 #endregion
 
 using System;
+using System.Diagnostics;
+using System.Numerics;
+using System.Threading.Tasks;
 
 namespace Fibonacci
 {
@@ -19,18 +22,79 @@ namespace Fibonacci
         /// </summary>
         /// <param name="n">value</param>
         /// <returns>Fibonacci number</returns>
-        public static long FibonacciBine_long(int n)
+        public static double FibonacciBine(int n)
         {
             // Square root of 5
             double sr5 = Math.Sqrt(5);
-            
+
             // Golden ratio value
-            double phi = 0.5 * (sr5 - 1);
+            double phi = 0.5 * (sr5 + 1);
+            double phi_n = checked(Math.Pow(phi, n));
 
             // Result
-            double res = (Math.Pow(phi + 1, n) - Math.Pow(-phi, n)) / sr5;
+            double res = (phi_n - Math.Pow(-1, n) / phi_n) / sr5;
 
-            return (long)Math.Round(res, MidpointRounding.AwayFromZero);
+            return Math.Round(res, MidpointRounding.AwayFromZero);
+        }
+
+        /// <summary>
+        /// Binet's formula
+        /// </summary>
+        /// <param name="array">array value</param>
+        /// <returns>array Fibonacci number</returns>
+        public static unsafe double[] FibonacciBine(int[] array)
+        {
+            // Square root of 5
+            double sr5 = Math.Sqrt(5);
+
+            // Golden ratio value
+            double phi = 0.5 * (sr5 + 1);
+
+            double[] phi_n = new double[array.Length],
+                res = new double[array.Length];
+
+            fixed (double* p = phi_n, r = res)
+            fixed (int* a = array)
+            {
+                double* _p = p, _r = r;
+                int* _a = a;
+
+                for (int i = 0; i < array.Length; i++, _p++, _a++, _r++)
+                {
+                    *_p = checked(Math.Pow(phi, *_a));
+                    *_r = (*_p - Math.Pow(-1, *_a) / *_p) / sr5;
+                    *_r = Math.Round(*_r, MidpointRounding.AwayFromZero);
+                }
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// Binet's formula using threads
+        /// </summary>
+        /// <param name="array">array value</param>
+        /// <returns>array Fibonacci number</returns>
+        public static double[] FibonacciBine_Par(int[] array)
+        {
+            // Square root of 5
+            double sr5 = Math.Sqrt(5);
+
+            // Golden ratio value
+            double phi = 0.5 * (sr5 + 1);
+
+            double[] res = new double[array.Length];
+
+            Parallel.For(0, array.Length, (i) =>
+            {
+                double phi_n = checked(Math.Pow(phi, array[i]));
+                // Result
+                double result = (phi_n - Math.Pow(-1, array[i]) / phi_n) / sr5;
+
+                res[i] = Math.Round(result, MidpointRounding.AwayFromZero);
+            });
+
+            return res;
         }
 
         /// <summary>
@@ -38,7 +102,7 @@ namespace Fibonacci
         /// </summary>
         /// <param name="n">value</param>
         /// <returns>Fibonacci number</returns>
-        public static long FibonacciApproximation_long(int n)
+        public static double FibonacciApproximation(int n)
         {
             // Square root of 5
             double sr5 = Math.Sqrt(5);
@@ -47,10 +111,68 @@ namespace Fibonacci
             double phi = 0.5 * (sr5 - 1);
 
             // Result
-            double res = ((n >= 0) ? Math.Pow(phi + 1, n) : -Math.Pow(-phi, n)) / sr5;
+            double res = checked(((n >= 0) ? Math.Pow(phi + 1, n) : -Math.Pow(-phi, n)) / sr5);
 
-            return (long)Math.Round(res, MidpointRounding.AwayFromZero);
+            return Math.Round(res, MidpointRounding.AwayFromZero);
         }
+
+        /// <summary>
+        /// Approximation formula
+        /// </summary>
+        /// <param name="array">array value</param>
+        /// <returns>array Fibonacci number</returns>
+        public static unsafe double[] FibonacciApproximation(int[] array)
+        {
+            // Square root of 5
+            double sr5 = Math.Sqrt(5);
+
+            // Golden ratio value
+            double phi = 0.5 * (sr5 - 1);
+
+            double[] res = new double[array.Length];
+
+            fixed (double* r = res)
+            fixed (int* a = array)
+            {
+                double* _r = r;
+                int* _a = a;
+
+                for (int i = 0; i < array.Length; i++, _a++, _r++)
+                {
+                    *_r = checked(((*_a >= 0) ? Math.Pow(phi + 1, *_a) : -Math.Pow(-phi, *_a)) / sr5);
+                    *_r = Math.Round(*_r, MidpointRounding.AwayFromZero);
+                }
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// Approximation formula using threads
+        /// </summary>
+        /// <param name="array">array value</param>
+        /// <returns>array Fibonacci number</returns>
+        public static double[] FibonacciApproximation_Par(int[] array)
+        {
+            // Square root of 5
+            double sr5 = Math.Sqrt(5);
+
+            // Golden ratio value
+            double phi = 0.5 * (sr5 - 1);
+
+            double[] res = new double[array.Length];
+
+            Parallel.For(0, array.Length, (i) =>
+            {
+                // Result
+                double result = checked(((array[i] >= 0) ? Math.Pow(phi + 1, array[i]) : -Math.Pow(-phi, array[i])) / sr5);
+
+                res[i] = Math.Round(result, MidpointRounding.AwayFromZero);
+            });
+
+            return res;
+        }
+
     }
 
 }
