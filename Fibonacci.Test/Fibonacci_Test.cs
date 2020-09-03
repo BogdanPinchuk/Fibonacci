7a20e917a82348f4132031a7a80dad6f7f697970
Bogdan <pinchuk.brus@windowslive.com>
2020-09-03 12:47:43 +0300

    Added exceptions, extention unit test, and use type BigInteger

--- Fibonacci.Test/Fibonacci_Test.cs
+++ Fibonacci.Test/Fibonacci_Test.cs
@@ -1,7 +1,11 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using System;
 using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Fibonacci.Test
 {
@@ -24,11 +28,111 @@ namespace Fibonacci.Test
                 return stub;
             }
         }
+
         private double[] Expected
             => new double[]
             {
                 -21, 13, -8, 5, -3, 2, -1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21,
                 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765
+            };
+        private ulong[] Big_Expected
+            => new ulong[]
+            {
+                0                   ,
+                1                   ,
+                1                   ,
+                2                   ,
+                3                   ,
+                5                   ,
+                8                   ,
+                13                  ,
+                21                  ,
+                34                  ,
+                55                  ,
+                89                  ,
+                144                 ,
+                233                 ,
+                377                 ,
+                610                 ,
+                987                 ,
+                1597                ,
+                2584                ,
+                4181                ,
+                6765                ,
+                10946               ,
+                17711               ,
+                28657               ,
+                46368               ,
+                75025               ,
+                121393              ,
+                196418              ,
+                317811              ,
+                514229              ,
+                832040              ,
+                1346269             ,
+                2178309             ,
+                3524578             ,
+                5702887             ,
+                9227465             ,
+                14930352            ,
+                24157817            ,
+                39088169            ,
+                63245986            ,
+                102334155           ,
+                165580141           ,
+                267914296           ,
+                433494437           ,
+                701408733           ,
+                1134903170          ,
+                1836311903          ,
+                2971215073          ,
+                4807526976          ,
+                7778742049          ,
+                12586269025         ,
+                20365011074         ,
+                32951280099         ,
+                53316291173         ,
+                86267571272         ,
+                139583862445        ,
+                225851433717        ,
+                365435296162        ,
+                591286729879        ,
+                956722026041        ,
+                1548008755920       ,
+                2504730781961       ,
+                4052739537881       ,
+                6557470319842       ,
+                10610209857723      ,
+                17167680177565      ,
+                27777890035288      ,
+                44945570212853      ,
+                72723460248141      ,
+                117669030460994     ,
+                190392490709135     ,
+                308061521170129     ,
+                498454011879264     ,
+                806515533049393     ,
+                1304969544928657    ,
+                2111485077978050    ,
+                3416454622906707    ,
+                5527939700884757    ,
+                8944394323791464    ,
+                14472334024676221   ,
+                23416728348467685   ,
+                37889062373143906   ,
+                61305790721611591   ,
+                99194853094755497   ,
+                160500643816367088  ,
+                259695496911122585  ,
+                420196140727489673  ,
+                679891637638612258  ,
+                1100087778366101931 ,
+                1779979416004714189 ,
+                2880067194370816120 ,
+                4660046610375530309 ,
+                7540113804746346429 ,
+                12200160415121876738,
+                // max value for ulong is 18446744073709551615, so max number is 93
             };
 
 
@@ -47,7 +151,7 @@ namespace Fibonacci.Test
         [TestMethod]
         public void FibonacciApproximation_ArrayValue()
             => Fibonacci_ArrayValue(Fibonacci.FibonacciApproximation);
-        
+
         [TestMethod]
         public void FibonacciBine_Parallel()
             => Fibonacci_ArrayValue(Fibonacci.FibonacciBine_Par);
@@ -55,6 +159,14 @@ namespace Fibonacci.Test
         [TestMethod]
         public void FibonacciApproximation_Parallel()
             => Fibonacci_ArrayValue(Fibonacci.FibonacciApproximation_Par);
+
+        [TestMethod]
+        public void FibonacciSlow_OneInputValue()
+            => Fibonacci_OneInputValue(Fibonacci.FibonacciSlow);
+
+        [TestMethod]
+        public void FibonacciFast_OneInputValue()
+            => Fibonacci_OneInputValue(Fibonacci.FibonacciFast);
 
 
         private void Fibonacci_OneInputValue(del_Fibonacci<double, int> fib)
@@ -84,7 +196,7 @@ namespace Fibonacci.Test
 
             Debug.WriteLine(str.ToString());
         }
-        
+
         private void Fibonacci_ArrayValue(del_Fibonacci<double[], int[]> fib)
         {
             // avarrage
@@ -111,28 +223,200 @@ namespace Fibonacci.Test
         }
 
 
+
         [TestMethod]
-        public void CompareFibMethods()
+        public void FibonacciBine_OneInputValue_Exception()
+            => Fibonacci_OneInputValueException_(Fibonacci.FibonacciBine);
+
+        [TestMethod]
+        public void FibonacciBine_ArrayValu_Exception()
+            => Fibonacci_ArrayValue_Exception(Fibonacci.FibonacciBine);
+
+        [TestMethod]
+        public void FibonacciApproximation_OneInputValue_Exception()
+            => Fibonacci_OneInputValueException_(Fibonacci.FibonacciApproximation);
+
+        [TestMethod]
+        public void FibonacciApproximation_ArrayValu_Exception()
+            => Fibonacci_ArrayValue_Exception(Fibonacci.FibonacciApproximation);
+
+
+
+        private void Fibonacci_OneInputValueException_(del_Fibonacci<double, int> fib)
         {
             // avarrage
-            int[] stub = new int[101];
+            int[] stub = new int[] { -71, 71 };
 
-            for (int i = 0; i < stub.Length; i++)
-                stub[i] = i;
+            string expected = "Absolute values more than 70 are not accurate. You should use other method.";
+
+            // act - assert
+            foreach (var i in stub)
+                Assert.ThrowsException<Exception>(() => fib(i), expected);
+        }
+
+        private void Fibonacci_ArrayValue_Exception(del_Fibonacci<double[], int[]> fib)
+        {
+            // avarrage
+            int[] stub = new int[] { -71, 71 };
+
+            string expected = "Absolute values more than 70 are not accurate. You should use other method.";
+
+            // act - assert
+            Assert.ThrowsException<Exception>(() => fib(stub), expected);
+        }
+
+
+        private void Fibonacci_OneInputValue(del_Fibonacci<BigInteger, int> fib)
+        {
+            // avarrage
+            var str = new StringBuilder("\nInput data:\n");
+            foreach (var i in Stub)
+                str.Append(i).Append("\t");
+
+            str.Append("\n\nExpected:\n");
+            foreach (var i in Expected)
+                str.Append(i).Append("\t");
+
+            // act
+            BigInteger[] actual = new BigInteger[Stub.Length];
+
+            for (int i = 0; i < Stub.Length; i++)
+                actual[i] = fib(Stub[i]);
+
+            str.Append("\n\nActual:\n");
+            foreach (var i in actual)
+                str.Append(i).Append("\t");
+
+            // assert
+            for (int i = 0; i < actual.Length; i++)
+                Assert.AreEqual((BigInteger)Expected[i], actual[i]);
+
+            Debug.WriteLine(str.ToString());
+        }
+
+
+        [TestMethod]
+        public void Fibonacci_Slow_OneInputValue_Par_0_93()
+            => Fibonacci_OneInputValue_Par_0_93(Fibonacci.FibonacciSlow);
+
+        [TestMethod]
+        public void Fibonacci_Fast_OneInputValue_Par_0_93()
+            => Fibonacci_OneInputValue_Par_0_93(Fibonacci.FibonacciFast);
+
+        private void Fibonacci_OneInputValue_Par_0_93(del_Fibonacci<BigInteger, int> fib)
+        {
+            // avarrage
+            var str = new StringBuilder("\nResult data:\n\n");
+
+            // act
+            BigInteger[] actual = new BigInteger[Big_Expected.Length];
+
+            Parallel.For(0, Big_Expected.Length, (i) => actual[i] = fib(i));
+
+            // assert
+            Parallel.For(0, Big_Expected.Length, (i) => Assert.AreEqual(Big_Expected[i], actual[i]));
+
+            // present
+            for (int i = 0; i < actual.Length; i++)
+                str.Append(i).Append(":\t").Append(actual[i]).Append("\n");
+
+            Debug.WriteLine(str);
+        }
+
+        /// <summary>
+        /// Method for testing max value before add exception,
+        /// for use this method, you should comment "throw" block and change private -> public
+        /// </summary>
+        [TestMethod]
+        private void MaxNumberWithAcurateValue()
+        {
+            // For present result
+            var str = new StringBuilder("\nResult data:\n\n");
+
+            // maximum number for other formula which dependes on acurate of "double" type
+            int maxN = 0;
+
+            BigInteger expected = default,
+                actual = default;
+
+            // for Binet's formula
+            for (maxN = 0; expected == actual; maxN++)
+            {
+                expected = Fibonacci.FibonacciSlow(maxN);
+                actual = (BigInteger)Fibonacci.FibonacciBine(maxN);
+            }
+
+            maxN -= 2;
+            expected = Fibonacci.FibonacciSlow(maxN);
+            actual = (BigInteger)Fibonacci.FibonacciBine(maxN);
+
+            str.Append("Acurate of Binet's formula:\n")
+                .Append($"\tmax number: {maxN};\n")
+                .Append($"\texpected value:\t{expected}\n")
+                .Append($"\tactual value:\t{actual}\n");
+
+            maxN++;
+            expected = Fibonacci.FibonacciSlow(maxN);
+            actual = (BigInteger)Fibonacci.FibonacciBine(maxN);
+
+            // next value
+            str.Append($"\n\tnext number: {maxN};\n")
+                .Append($"\texpected value:\t{expected}\n")
+                .Append($"\tactual value:\t{actual}\n");
+
+            expected = actual = default;
+
+            // for Approximation formula
+            for (maxN = 0; Equals(expected, actual); maxN++)
+            {
+                expected = Fibonacci.FibonacciSlow(maxN);
+                actual = (BigInteger)Fibonacci.FibonacciApproximation(maxN);
+            }
+
+            maxN -= 2;
+            expected = Fibonacci.FibonacciSlow(maxN);
+            actual = (BigInteger)Fibonacci.FibonacciApproximation(maxN);
+
+            str.Append("\nAcurate of Approximation formula:\n")
+                .Append($"\tmax number: {maxN};\n")
+                .Append($"\texpected value:\t{expected}\n")
+                .Append($"\tactual value:\t{actual}\n");
+
+            maxN++;
+            expected = Fibonacci.FibonacciSlow(maxN);
+            actual = (BigInteger)Fibonacci.FibonacciBine(maxN);
+
+            // next value
+            str.Append($"\n\tnext number: {maxN};\n")
+                .Append($"\texpected value:\t{expected}\n")
+                .Append($"\tactual value:\t{actual}\n");
+
+            // present
+            Debug.WriteLine(str);
+        }
+
+        [TestMethod]
+        public void CompareSpeedMethods()
+        {
+            // avarrage
+            int[] stub = Enumerable
+                .Range(0, 71)
+                .ToArray();
 
             // act
             Stopwatch timer = new Stopwatch();
 
             timer.Start();
-            for (int i = 0; i < stub.Length; i++)
-                Fibonacci.FibonacciBine(stub[i]);
+            foreach (var i in stub)
+                Fibonacci.FibonacciBine(i);
             timer.Stop();
 
             Debug.WriteLine($"\nBinet's formula: {timer.Elapsed.TotalMilliseconds} ms\n");
 
             timer.Restart();
-            for (int i = 0; i < stub.Length; i++)
-                Fibonacci.FibonacciApproximation(stub[i]);
+            foreach (var i in stub)
+                Fibonacci.FibonacciApproximation(i);
+
             timer.Stop();
             Debug.WriteLine($"Approximation formula: {timer.Elapsed.TotalMilliseconds} ms\n");
 
@@ -156,6 +440,29 @@ namespace Fibonacci.Test
             timer.Stop();
             Debug.WriteLine($"Approximation formula (parallel): {timer.Elapsed.TotalMilliseconds} ms\n");
 
+            timer.Restart();
+            foreach (var i in stub)
+                Fibonacci.FibonacciSlow(i);
+            timer.Stop();
+            Debug.WriteLine($"Base calculate: {timer.Elapsed.TotalMilliseconds} ms\n");
+
+            timer.Restart();
+            foreach (BigInteger i in stub)
+                Fibonacci.FibonacciSlow(i);
+            timer.Stop();
+            Debug.WriteLine($"Base calculate (BigInteger): {timer.Elapsed.TotalMilliseconds} ms\n");
+
+            timer.Restart();
+            foreach (var i in stub)
+                Fibonacci.FibonacciFast(i);
+            timer.Stop();
+            Debug.WriteLine($"Fast calculate: {timer.Elapsed.TotalMilliseconds} ms\n");
+
+            timer.Restart();
+            foreach (BigInteger i in stub)
+                Fibonacci.FibonacciFast(i);
+            timer.Stop();
+            Debug.WriteLine($"Fast calculate (BigInteger): {timer.Elapsed.TotalMilliseconds} ms\n");
         }
 
     }
--- Fibonacci/Fibonacci.cs
+++ Fibonacci/Fibonacci.cs
@@ -6,8 +6,11 @@
 #endregion
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Numerics;
+using System.Reflection.Metadata;
 using System.Threading.Tasks;
 
 namespace Fibonacci
@@ -17,6 +20,8 @@ namespace Fibonacci
     /// </summary>
     public static class Fibonacci
     {
+        private const string error = "Absolute values more than 70 are not accurate. You should use other method.";
+
         /// <summary>
         /// Binet's formula
         /// </summary>
@@ -24,6 +29,14 @@ namespace Fibonacci
         /// <returns>Fibonacci number</returns>
         public static double FibonacciBine(int n)
         {
+            // fast answer
+            if (n == 0)
+                return 0;
+            else if (Math.Abs(n) == 1)
+                return 1;
+            else if (Math.Abs(n) > 70)
+                throw new Exception(error);
+
             // Square root of 5
             double sr5 = Math.Sqrt(5);
 
@@ -44,6 +57,10 @@ namespace Fibonacci
         /// <returns>array Fibonacci number</returns>
         public static unsafe double[] FibonacciBine(int[] array)
         {
+            // accurate
+            if (array.Max() > 70 || array.Min() < -70)
+                throw new Exception(error);
+
             // Square root of 5
             double sr5 = Math.Sqrt(5);
 
@@ -61,6 +78,18 @@ namespace Fibonacci
 
                 for (int i = 0; i < array.Length; i++, _p++, _a++, _r++)
                 {
+                    // fast answer
+                    if (*_a == 0)
+                    {
+                        *_r = 0;
+                        continue;
+                    }
+                    else if (Math.Abs(*_a) == 1)
+                    {
+                        *_r = 1;
+                        continue;
+                    }
+
                     *_p = checked(Math.Pow(phi, *_a));
                     *_r = (*_p - Math.Pow(-1, *_a) / *_p) / sr5;
                     *_r = Math.Round(*_r, MidpointRounding.AwayFromZero);
@@ -71,12 +100,16 @@ namespace Fibonacci
         }
 
         /// <summary>
-        /// Binet's formula using threads
+        /// Binet's formula using parallel calculations
         /// </summary>
         /// <param name="array">array value</param>
         /// <returns>array Fibonacci number</returns>
         public static double[] FibonacciBine_Par(int[] array)
         {
+            // accurate
+            if (array.Max() > 70 || array.Min() < -70)
+                throw new Exception(error);
+
             // Square root of 5
             double sr5 = Math.Sqrt(5);
 
@@ -87,6 +120,12 @@ namespace Fibonacci
 
             Parallel.For(0, array.Length, (i) =>
             {
+                // fast answer
+                if (array[i] == 0)
+                    res[i] = 0;
+                else if (Math.Abs(array[i]) == 1)
+                    res[i] = 1;
+
                 double phi_n = checked(Math.Pow(phi, array[i]));
                 // Result
                 double result = (phi_n - Math.Pow(-1, array[i]) / phi_n) / sr5;
@@ -104,6 +143,14 @@ namespace Fibonacci
         /// <returns>Fibonacci number</returns>
         public static double FibonacciApproximation(int n)
         {
+            // fast answer
+            if (n == 0)
+                return 0;
+            else if (Math.Abs(n) == 1)
+                return 1;
+            else if (Math.Abs(n) > 70)
+                throw new Exception(error);
+
             // Square root of 5
             double sr5 = Math.Sqrt(5);
 
@@ -123,6 +170,10 @@ namespace Fibonacci
         /// <returns>array Fibonacci number</returns>
         public static unsafe double[] FibonacciApproximation(int[] array)
         {
+            // accurate
+            if (array.Max() > 70 || array.Min() < -70)
+                throw new Exception(error);
+
             // Square root of 5
             double sr5 = Math.Sqrt(5);
 
@@ -139,6 +190,18 @@ namespace Fibonacci
 
                 for (int i = 0; i < array.Length; i++, _a++, _r++)
                 {
+                    // fast answer
+                    if (*_a == 0)
+                    {
+                        *_r = 0;
+                        continue;
+                    }
+                    else if (Math.Abs(*_a) == 1)
+                    {
+                        *_r = 1;
+                        continue;
+                    }
+
                     *_r = checked(((*_a >= 0) ? Math.Pow(phi + 1, *_a) : -Math.Pow(-phi, *_a)) / sr5);
                     *_r = Math.Round(*_r, MidpointRounding.AwayFromZero);
                 }
@@ -148,12 +211,16 @@ namespace Fibonacci
         }
 
         /// <summary>
-        /// Approximation formula using threads
+        /// Approximation formula using parallel calculations
         /// </summary>
         /// <param name="array">array value</param>
         /// <returns>array Fibonacci number</returns>
         public static double[] FibonacciApproximation_Par(int[] array)
         {
+            // accurate
+            if (array.Max() > 70 || array.Min() < -70)
+                throw new Exception(error);
+
             // Square root of 5
             double sr5 = Math.Sqrt(5);
 
@@ -164,6 +231,12 @@ namespace Fibonacci
 
             Parallel.For(0, array.Length, (i) =>
             {
+                // fast answer
+                if (array[i] == 0)
+                    res[i] = 0;
+                else if (Math.Abs(array[i]) == 1)
+                    res[i] = 1;
+
                 // Result
                 double result = checked(((array[i] >= 0) ? Math.Pow(phi + 1, array[i]) : -Math.Pow(-phi, array[i])) / sr5);
 
@@ -171,6 +244,201 @@ namespace Fibonacci
             });
 
             return res;
+        }
+
+        /// <summary>
+        /// Slow calculate but accurate result, can calculate absolute value more 70
+        /// </summary>
+        /// <param name="n">value</param>
+        /// <returns>Fibonacci number</returns>
+        public static unsafe BigInteger FibonacciSlow(int n)
+        {
+            // absolutely value of n 
+            int value = Math.Abs(n);
+
+            // sing of result : Math.Pow(-1, value + 1)
+            int sign = (value % 2 == 0) ? -1 : 1;
+
+            // fast answer or start data for calculate
+            if (value == 0)
+                return 0;
+            else if (value == 1)
+                return 1;
+            else if (value == 2)
+                return (n > 0) ? 1 : -1;
+
+            #region Explanation
+            /*
+            0   1   2   3   4   5   6   7   -> № (number of sequence)
+            0   1   1   2   3   5   8   13  -> FN (fibonacci number)
+
+            1. we have values: FN[1, 1, 1] -> №[1, 2, 2]
+            2. we get (remove): FN{1, 1} -> №{1, 2}
+            3. balance: FN[1] -> №[2]
+            4. calculate next value: FN(2) -> №(3)
+            5. we set (insert): FN{2, 2} -> №{3, 3}
+            6. we have: FN[1, 2, 3] -> №[2, 3, 3]
+            
+            7. repeat 1-6 for to get necessary result
+
+            */
+            #endregion
+
+            Queue<BigInteger> neighbor = new Queue<BigInteger>();
+
+            // start value
+            for (int i = 0; i < 3; i++)
+                neighbor.Enqueue(1);
+
+            // calculate
+            BigInteger result = default;
+
+            for (int i = 0; i < value - 2; i++)
+            {
+                result = neighbor.Dequeue() + neighbor.Dequeue();
+                neighbor.Enqueue(result);
+                neighbor.Enqueue(result);
+            }
+
+            return (n > 0) ? result : sign * result;
+        }
+
+        /// <summary>
+        /// Slow calculate but accurate result, can calculate absolute value more 70
+        /// </summary>
+        /// <param name="n">value</param>
+        /// <returns>Fibonacci number</returns>
+        public static unsafe BigInteger FibonacciSlow(BigInteger n)
+        {
+            // absolutely value of n 
+            BigInteger value = BigInteger.Abs(n);
+
+            // sing of result : Math.Pow(-1, value + 1)
+            int sign = (value % 2 == 0) ? -1 : 1;
+
+            // fast answer or start data for calculate
+            if (value == 0)
+                return 0;
+            else if (value == 1)
+                return 1;
+            else if (value == 2)
+                return (n > 0) ? 1 : -1;
+
+            Queue<BigInteger> neighbor = new Queue<BigInteger>();
+
+            // start value
+            for (int i = 0; i < 3; i++)
+                neighbor.Enqueue(1);
+
+            // calculate
+            BigInteger result = default;
+
+            for (BigInteger i = 0; i < value - 2; i++)
+            {
+                result = neighbor.Dequeue() + neighbor.Dequeue();
+                neighbor.Enqueue(result);
+                neighbor.Enqueue(result);
+            }
+
+            return (n > 0) ? result : sign * result;
+        }
+
+        /// <summary>
+        /// Slow calculate but accurate result, can calculate absolute value more 70
+        /// </summary>
+        /// <param name="n">value</param>
+        /// <returns>Fibonacci number</returns>
+        public static unsafe BigInteger FibonacciFast(int n)
+        {
+            // absolutely value of n 
+            int value = Math.Abs(n);
+
+            // fast answer or start data for calculate
+            if (value == 0)
+                return 0;
+            else if (value == 1)
+                return 1;
+            else if (value == 2)
+                return (n > 0) ? 1 : -1;
+
+            // sing of result : Math.Pow(-1, value + 1)
+            int sign = (value % 2 == 0) ? -1 : 1;
+
+            if (value <= 70)
+                return (BigInteger)((n > 0) ? 
+                    FibonacciApproximation(value) : sign * FibonacciApproximation(value));
+
+            Queue<BigInteger> neighbor = new Queue<BigInteger>();
+
+            // start value
+            {
+                BigInteger temp = (BigInteger)FibonacciApproximation(69);
+                neighbor.Enqueue(temp);
+                temp = (BigInteger)FibonacciApproximation(70);
+                neighbor.Enqueue(temp);
+                neighbor.Enqueue(temp);
+            }
+
+            // calculate use base method
+            BigInteger result = default;
+
+            for (int i = 70; i < value; i++)
+            {
+                result = neighbor.Dequeue() + neighbor.Dequeue();
+                neighbor.Enqueue(result);
+                neighbor.Enqueue(result);
+            }
+
+            return (n > 0) ? result : sign * result;
+        }
+
+        /// <summary>
+        /// Slow calculate but accurate result, can calculate absolute value more 70
+        /// </summary>
+        /// <param name="n">value</param>
+        /// <returns>Fibonacci number</returns>
+        public static unsafe BigInteger FibonacciFast(BigInteger n)
+        {
+            // absolutely value of n 
+            BigInteger value = BigInteger.Abs(n);
+
+            // fast answer or start data for calculate
+            if (value == 0)
+                return 0;
+            else if (value == 1)
+                return 1;
+            else if (value == 2)
+                return (n > 0) ? 1 : -1;
+
+            // sing of result : Math.Pow(-1, value + 1)
+            int sign = (value % 2 == 0) ? -1 : 1;
+
+            if (value <= 70)
+                return (BigInteger)((n > 0) ?
+                    FibonacciApproximation((int)value) : sign * FibonacciApproximation((int)value));
+
+            Queue<BigInteger> neighbor = new Queue<BigInteger>();
+
+            // start value
+            {
+                BigInteger temp = (BigInteger)FibonacciApproximation(69);
+                neighbor.Enqueue(temp);
+                temp = (BigInteger)FibonacciApproximation(70);
+                neighbor.Enqueue(temp);
+                neighbor.Enqueue(temp);
+            }
+
+            // calculate use base method
+            BigInteger result = default;
+
+            for (BigInteger i = 70; i < value; i++)
+            {
+                result = neighbor.Dequeue() + neighbor.Dequeue();
+                neighbor.Enqueue(result);
+                neighbor.Enqueue(result);
+            }
+
+            return (n > 0) ? result : sign * result;
         }
 
     }
